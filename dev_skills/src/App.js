36f44fc0fa2286b87cc29f4ebe533b85e8c95689
jbarcs92 <jbarcl92@gmail.com>
2023-07-21 16:45:15 -0400

    Part 3 Submission

--- dev_skills/src/App.js
+++ dev_skills/src/App.js
@@ -1,19 +1,20 @@
+import { useState } from "react";
 import SkillList from "./SkillList.jsx";
 import NewSkillForm from "./NewSkillForm.jsx";
 import "./App.css";
 
-const skills = [
-  { name: "HTML", level: 5 },
-  { name: "CSS", level: 3 },
-  { name: "JavaScript", level: 4 },
-  { name: "Python", level: 2 },
-];
-
 export default function App() {
+  const [skills, setSkills] = useState([
+    { name: "HTML", level: 5 },
+    { name: "CSS", level: 3 },
+    { name: "JavaScript", level: 4 },
+    { name: "Python", level: 2 },
+  ]);
+  const [showSkills, setShowSkills] = useState(true);
   return (
     <div className="App teal-text padding-0">
       <h1>React Dev Skills</h1>
-      <SkillList skills={skills} />
+      {showSkills && <SkillList skills={skills} />}
       <hr></hr>
       <NewSkillForm />
     </div>
